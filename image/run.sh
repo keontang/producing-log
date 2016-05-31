995aa8fa28524853b2caf9ec1bdd4249af176b10
keontang <ikeontang@gmail.com>
2016-05-30 21:44:05 -0400

    update

--- image/run.sh
+++ image/run.sh
@@ -2,6 +2,8 @@
 
 while true
 do
-  echo "[`date`]: for logging test"
-  sleep 20
+  #echo "[`TZ='Asia/Shanghai' date`]: For logging test v1: Every day is a good day."
+  #echo "[`TZ='Asia/Shanghai' date`]: For logging test v2: Welcome to Caicloud Kubernetes."
+  echo "[`TZ='Asia/Shanghai' date`]: For logging test v3: Welcome to Hello, my friends."
+  sleep 25
 done
